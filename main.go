2e628dbf505c874c5ab160e7341f723eab650e89
carolina-guzman <carolina.guzman5480@alumnos.udg.mx>
2020-10-10 17:06:43 -0500

    Requerimientos base

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var n uint64
+	var i uint64
+	fmt.Scan(&n)
+	var r int
+	var numero int
+	var s [] int
+	for i= 0; i < n; i++ {
+		fmt.Scan(&numero)
+		s = append(s, numero)
+		//fmt.Println(s[i])
+		r += s[i]
+	}
+	fmt.Println(r)
+
+
 }
